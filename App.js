b233dc9b3f9a63835ec5d5e20472f44f3938ae96
Arriziq <114041758+Arriziqrzyx@users.noreply.github.com>
2024-06-14 02:46:46 +0700

    Adding Buttons & TextInput

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import CustomButton from './src/components/customButton';
+import CustomTextInput from './src/components/customTextInput';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <CustomButton
+      backgroundColor="#DDDDDD"
+      color="#39494F"
+      text="Custom Button"
+      width="100%"
+      onPress={() => {}}
+    />
+      <CustomTextInput 
+      label="Custom Text"
+      multiline
+      numberOfLines={2}
+      onChange={() => {}}
+      />
     </View>
   );
 }
@@ -13,8 +26,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    display: 'flex',
+    flexDirection: 'column',
     justifyContent: 'center',
+    padding: 40,
   },
 });
---
+++ src/components/customTextInput.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { TextInput, Text, StyleSheet, View } from 'react-native'
+
+function CustomTextInput({
+  text,
+  onChange,
+  label,
+  multiline,
+  numberOfLines,
+}) {
+  return (
+    <View style={styles.textInputWrapper}>
+      <Text>{label}</Text>
+      <TextInput
+        multiline={multiline}
+        numberOfLines={numberOfLines}
+        style={styles.input}
+        placeholder={label}
+        onChangeText={onChange}
+        defaultValue={text}
+      />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  textInputWrapper: {
+    marginTop: 20,
+  },
+  input: {
+    borderWidth: 2,
+    borderColor: '#DDD',
+    padding: 10,
+  },
+})
+
+export default CustomTextInput
